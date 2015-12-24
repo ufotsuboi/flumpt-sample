290aae9626c4f70a81a52eb06ef06a2e7054a93a
yuuho.tsuboi <yuuho.tsuboi@adish.co.jp>
2015-12-24 14:52:09 +0900

    create gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+import gulp from 'gulp';
+import watch from 'gulp-watch';
+import webpack from 'gulp-webpack';
+
+let config = {
+  webpack: {
+    entry: {
+      bundle: './src/js/app.js'
+    },
+    output: {
+      path: __dirname + '/dist/',
+      filename: '[name].js'
+    },
+    resolve: {
+      extensions: ['', '.js']
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader'
+        }
+      ]
+    },
+  }
+};
+
+gulp.task('webpack', () => {
+  gulp.src('src/js/app.js')
+      .pipe(webpack(config.webpack))
+      .pipe(gulp.dest('dist/bundle.js'));
+});
+
+gulp.task('watch', () => {
+  watch('src/js/**/*.js', () => {
+    gulp.start(['webpack']);
+  });
+});
+
+gulp.task('default', ['webpack', 'watch']);
